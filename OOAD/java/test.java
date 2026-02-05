c8ea08972a7cc4b0289b08e6f3b9146513626141
rtk5 <rithvik.matta@gmail.com>
2026-02-05 12:42:08 +0530

    5th feb

---
+++ OOAD/java/test.java
@@ -0,0 +1,30 @@
+class Car {
+    String brand;
+    String model;
+    int year;
+
+    Car(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    void displayInfo() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+    }
+}
+
+public class test {
+    public static void main(String[] args) {
+        Car car1 = new Car("Toyota", "Camry", 2020);
+        Car car2 = new Car("Honda", "Civic", 2019);
+
+        System.out.println("Car 1 Information:");
+        car1.displayInfo();
+
+        System.out.println("\nCar 2 Information:");
+        car2.displayInfo();
+    }
+}
